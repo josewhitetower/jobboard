d7ed8f96f2ba101efd013da751286cf5a29fa123
Jose Torreblanca <josewhitetower@gmail.com>
2017-12-17 17:34:37 +0100

    Some styles

--- client/src/components/JobBoard.vue
+++ client/src/components/JobBoard.vue
@@ -23,7 +23,7 @@
           <div v-for="job in jobs" :key="job.id" class="job">
             <v-layout>
               <v-flex >
-                <div class="job-title">     {{job.title}}</div>
+                <div class="job-title ">     {{job.title}}</div>
                 <div class="job-company">     {{job.company}}</div>
                 <div class="job-description">     {{job.description}}</div>
                   <v-btn 
@@ -72,19 +72,21 @@ export default {
   border-bottom: 2px solid cyan;
   margin-top: 20px;
   padding: 10px;
-  height: 200px;
+  /* height: 200px; */
 
 }
-.job-title{
+.job-title{  
   font-size: 30px;
   text-align: left;
 }
 .job-company{
+  margin-top: 10px;
   text-align: left;
   font-size: 24px;
 }
 .job-description {
+  margin-top: 7px;
   font-size: 18px;
-  text-align: center;
+  text-align: justify;
 }
 </style>
